480afd545c43234f27b88f0523a5ec82178e7174
thomasthaddeus <thaddeus.r.thomas@gmail.com>
2023-11-20 09:44:56 -0700

    change folder

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from routes import *
+from models import *
+from forms import *
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Import routes, models here
-from app import routes, models
 
 if __name__ == '__main__':
     app.run(debug=True)
